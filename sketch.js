0dff34e521592ab209e9c7f32c1d3a5e76a23df8
Kapilast77 <85858757+Kapilast77@users.noreply.github.com>
2021-08-20 19:58:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,7 +10,9 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var ground;
-
+var rope;
+var fruit;
+var link;
 function setup() 
 {
   createCanvas(500,700);
@@ -18,18 +20,24 @@ function setup()
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(200,680,600,20);
-
+  rope = new Rope(6,{x:245,y:30})
   rectMode(CENTER);
   ellipseMode(RADIUS);
   textSize(50)
-  
+  var fruitoptions={
+    density:0.001
+  }
+  fruit=Bodies.circle(300,300,15,fruitoptions)
+  Matter.Composite.add(rope.body,fruit)
+  link= new Link(rope,fruit)
 }
 
 function draw() 
 {
   background(51);
   ground.show();
-  
+  rope.show();
+  ellipse(fruit.position.x,fruit.position.y,15,15 )
   Engine.update(engine);
   
 
